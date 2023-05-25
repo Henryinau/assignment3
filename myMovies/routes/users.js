86a83d4e53954a83eb7e98179c295c2b5b1c1353
Henry <1153426669@qq.com>
2023-05-26 03:01:26 +1000

    user register

--- myMovies/routes/users.js
+++ myMovies/routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -15,6 +16,41 @@ router.post('/login', function(req, res, next) {
     token_type: "Bearer",
     expires_in
   });
+});
+
+router.post('/register', function(req, res, next) { 
+  // Retrieve email and password from req.body
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Verify body
+  if (!email || !password) {
+    res.status(400).json({
+      error: true,
+      message: "Request body incomplete - email and password needed"
+    });
+    return;
+  }
+
+  // Determine if user already exists in table
+  const queryUsers = req.db.from("users").select("*").where("email", "=", email);
+  queryUsers.then(users => {
+    if (users.length > 0) {
+      throw new Error("User already exists");
+    }
+
+    // Insert user into DB
+    const saltRounds = 10;
+    const password = bcrypt.hashSync(password, saltRounds);
+    return req.db.from("users").insert({ email, password });
+  })
+  .then(() => {
+    res.status(201).json({ success: true, message: "User created" });
+  })
+  .catch(e => {
+    res.status(500).json({ success: false, message: e.message });
+  });
+    
 });
 
 module.exports = router;
