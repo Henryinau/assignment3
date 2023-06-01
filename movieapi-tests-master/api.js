bac18d347e25b5401230bd077eb5df0a92f92353
Henry <1153426669@qq.com>
2023-06-01 22:10:31 +1000

    changes

---
+++ movieapi-tests-master/api.js
@@ -0,0 +1,3 @@
+//If you are serving your server on any port other than 3000, change the port here, or alternatively change the url to approriate
+const REMOTE_API_URL = ` http://sefdb02.qut.edu.au:3001`;
+module.exports = REMOTE_API_URL;
